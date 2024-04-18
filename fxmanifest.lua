a93282a80f3987e6497305ecee5857cfc57a7062
Valdarix Games <james@valdarixgames.com>
2024-04-18 12:48:12 -0400

    Update fxmanifest.lua
    Fixed Typo in the Manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'ValdarixGames'
-description 'Basic script to turn of the GTA Radio when a player enters a car.'
+description 'Basic script to turn off the GTA Radio when a player enters a car.'
 version '0.0.1'
 
 client_scripts {
